37e59d2be6871926faee51cc5d2184469e5b9985
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-08-16 13:26:08 +0300

    add db

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+path_db = os.path.join(BASE_DIR, 'src', 'capitalize.db')
--- src/parser.py
+++ src/parser.py
@@ -1,64 +1,57 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv
 from fake_headers import Headers
+from tqdm import tqdm
 
-load_dotenv()
 
-# API_TOKEN = os.getenv('API_TOKEN')
-# headers = {
-#     'Authorization': f'Bearer {API_TOKEN}',
-#     'Accept-Language': 'ru'
-# }
-#
-# search_words = 'search-regions'
-# params = {
-#     'searchTerm': 'Москва'
-# }
-#
-# URL = f'https://data-api.oxilor.com/rest/{search_words}'
+def parse():
+    """ Парсинг стран и городов. """
+    full_info = []
+    URL = 'https://infourok.ru/spisok-stran-i-ih-stolic-5710813.html'
 
-URL = 'https://infourok.ru/spisok-stran-i-ih-stolic-5710813.html'
+    headers = {
+        'user-agent': Headers(os="win", headers=True).generate()['User-Agent']
+    }
 
-headers = {
-    'user-agent': Headers(os="win", headers=True).generate()['User-Agent']
-}
+    response = requests.get(URL, headers=headers)
 
-params = None
+    soup = BeautifulSoup(response.text, 'lxml')
 
-response = requests.get(URL, headers=headers)
+    table = soup.find('table', attrs={'class': 'MsoNormalTable'})
+    cities_countries = table.find_all('tr')
 
-soup = BeautifulSoup(response.text, 'lxml')
+    for el in tqdm(cities_countries[1:]):
+        countries = el.find('p', attrs={'class': 'MsoNormal'}).text
+        countries_url = el.find('a')['href']
+        cities = el.find('p',
+                         attrs={'class': 'MsoNormal', 'align': 'center'}).text
 
-table = soup.find('table', attrs={'class': 'MsoNormalTable'})
-cities_countries = table.find_all('tr')
+        detail_response = requests.get(countries_url, headers=headers)
+        detail_soup = BeautifulSoup(detail_response.text, 'lxml')
+        detail_table = detail_soup.find('div', attrs={'class': 'info'})
+        country_details = detail_table.find_all('dd')
 
-for el in cities_countries[1:]:
-    countries = el.find('p', attrs={'class': 'MsoNormal'}).text
-    countries_url = el.find('a')['href']
-    cities = el.find('p', attrs={'class': 'MsoNormal', 'align': 'center'}).text
-    # print(f'{countries}: {cities}\n{countries_url}')
+        try:
+            area = country_details[3].contents[1].strip()
+            people = country_details[4].contents[1].strip()
+            official_language = country_details[8].text
+            valuta = country_details[9].text
+            phone_code = country_details[10].text
+            image = f'http://ostranah.ru/{detail_soup.find_all('img')[1]['src']}'
+        except IndexError:
+            area = country_details[4].contents[1].strip()
+            people = country_details[5].contents[1].strip()
+            official_language = country_details[9].text
+            valuta = country_details[10].text
+            phone_code = country_details[11].text
+            image = f'http://ostranah.ru/{detail_soup.find_all('img')[1]['src']}'
 
-    detail_response = requests.get(countries_url, headers=headers)
-    detail_soup = BeautifulSoup(detail_response.text, 'lxml')
-    detail_table = detail_soup.find('div', attrs={'class': 'info'})
-    country_details = detail_table.find_all('dd')
+        info = (countries, cities, area, people, official_language,
+                valuta, phone_code, countries_url, image)
+        full_info.append(info)
+    return full_info
 
-    try:
-        area = country_details[3].contents[1].strip()
-        people = country_details[4].contents[1].strip()
-        official_language = country_details[8].text
-        valuta = country_details[9].text
-        phone_code = country_details[10].text
-        image = f'http://ostranah.ru/{detail_soup.find_all('img')[1]['src']}'
-    except IndexError:
-        area = country_details[4].contents[1].strip()
-        people = country_details[5].contents[1].strip()
-        official_language = country_details[9].text
-        valuta = country_details[10].text
-        phone_code = country_details[11].text
-        image = f'http://ostranah.ru/{detail_soup.find_all('img')[1]['src']}'
 
-    print(
-        f'{countries}: {cities}\nArea: {area}\nPopulation: {people}\nOfficial language: {official_language}\nValuta: {valuta}\nPhone code: {phone_code}\n{countries_url}\n{image}\n')
+if __name__ == '__main__':
+    parse()
